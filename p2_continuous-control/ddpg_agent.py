2b1cf913d074073fa0694d4bdc31ef06cc888ce1
Mayur Andulkar <mayurv.andulkar@gmail.com>
2019-01-06 20:45:06 +0100

    reafct: Working code for single agent training using DDPG

--- p2_continuous-control/ddpg_agent.py
+++ p2_continuous-control/ddpg_agent.py
@@ -18,6 +18,9 @@ LR_CRITIC = 3e-4      # Critic Learning Rate
 WEIGHT_DECAY = 0.0001 # L2 weight decay
 BUFFER_SIZE = int(1e6)# replay buffer size
 BATCH_SIZE = 128      # minibatch size
+GAMMA = 0.99          # Gamma for discounting rewards
+TAU = 1e-3            # for soft update of target parameters
+UPDATE_EVERY = 2      # how often to update the local network
 
 class Agent():
     """Interacts with and learns from the environment"""
@@ -36,6 +39,7 @@ class Agent():
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(random_seed)
+        self.t_step = 0 # Time steps for updating the local actor
         
         # Actor Network (w/ Target Network)
         self.actor_local = Actor(state_size, action_size, random_seed).to(device)
@@ -53,6 +57,16 @@ class Agent():
 
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        
+    def step(self, state, action, reward, next_state, done):
+        """Save experience in replay memory, and use random sample from buffer to learn."""
+        # Save experience / reward
+        self.memory.add(state, action, reward, next_state, done)
+
+        # Learn, if enough samples are available in memory
+        if len(self.memory) > BATCH_SIZE:
+            experiences = self.memory.sample()
+            self.learn(experiences, GAMMA)
     
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
@@ -60,7 +74,12 @@ class Agent():
         self.actor_local.eval()
         with torch.no_grad():
             action = self.actor_local(state).cpu().data.numpy()
-        #self.actor_local.train()
+            
+        # Learn every UPDATE_EVERY time steps.
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        if self.t_step == 0:
+            self.actor_local.train() # Train the local actor every time step
+            
         if add_noise:
             action += self.noise.sample()
         return np.clip(action, -1, 1)
@@ -69,11 +88,76 @@ class Agent():
         self.noise.reset()
     
     
-    def learn(self):
-        pass
+    def learn(self, experiences, gamma):
+        """Update policy and value parameters using given batch of experience tuples.
+        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
+        where:
+            actor_target(state) -> action
+            critic_target(state, action) -> Q-value
+
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            gamma (float): discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+        # ---------------------------- update critic ---------------------------- #
+        # Get predicted next-state actions and Q values from target models
+        actions_next = self.actor_target(next_states)
+        Q_targets_next = self.critic_target(next_states, actions_next)
+        
+        # Compute Q targets for current states (y_i)
+        Q_targets = rewards + (gamma*Q_targets_next*(1-dones))
+        
+        # Compute critic loss
+        Q_expected = self.critic_local(states,actions)
+        critic_loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+        
     
-    def soft_update(self):
-        pass
+        # ---------------------------- update actor ---------------------------- #
+        # Compute actor loss
+        actions_pred = self.actor_local(states)
+        actor_loss = -self.critic_local(states,actions_pred).mean()
+        # Minimize the loss
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+    
+     # ----------------------- update target networks ----------------------- #
+        self.soft_update(self.critic_local, self.critic_target, TAU)
+        self.soft_update(self.actor_local, self.actor_target, TAU)                     
+
+    
+    def soft_update(self, local_model, target_model, tau):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+
+        Params
+        ======
+            local_model: PyTorch model (weights will be copied from)
+            target_model: PyTorch model (weights will be copied to)
+            tau (float): interpolation parameter 
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+
+            
+class NormalActionNoise:
+    def __init__(self, mu=0., sigma=0.2):
+        self.mu = mu
+        self.sigma = sigma
+
+    def __call__(self):
+        return np.random.normal(self.mu, self.sigma)
+
+    def __repr__(self):
+        return 'NormalActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)
+
+
     
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
